f91a7ff1600c19b76a0b08100b75c8191ddf6e7a
jcombs24 <jcombs3116@gmail.com>
2015-03-28 18:01:17 -0500

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                                 password_confirmation: "password" }
         end
         assert_template 'users/show'
-        assert_not flash.nil?
+        assert is_logged_in?
+        
       end
 end
